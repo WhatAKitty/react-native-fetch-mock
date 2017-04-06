bae69c344a10f8926767000adcb120ffd34a4a23
104xuqiang <104xuqiang@gmail.com>
2017-04-06 23:54:29 +0800

    add test with parameters

--- __mocks__/index.js
+++ __mocks__/index.js
@@ -1,17 +1,36 @@
 
 export default {
-  '/api/users': async () => {
+  '/api/users': async (options) => {
+    const all = [
+      {
+        name: 'John',
+        age: 15,
+      },
+      {
+        name: 'Lily',
+        age: 16,
+      }
+    ];
+    let filtered;
+    if ('undefined' !== typeof options) {
+      filtered = all.filter(item => {
+        let result = true;
+        const keys = Object.keys(options);
+        keys.forEach(key => {
+          const option = options[key];
+
+          if (item[key] && item[key] !== option) {
+            result = false;
+          }
+        });
+        
+        return result;
+      });
+    } else {
+      filtered = all;
+    }
     return {
-      data: [
-        {
-          name: 'John',
-          age: 15,
-        },
-        {
-          name: 'Lily',
-          age: 16,
-        }
-      ]
+      data: filtered,
     }
   }
 }
